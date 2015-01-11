ba9989315af684e9fd734d974f2e2d85cc2cdeae
InfinityDeltaX <delta0357@gmail.com>
2015-01-11 18:16:27 -0500

    Added a verb constructor and the Conjugated classes.

--- LatinGenerator/Verb.java
+++ LatinGenerator/Verb.java
@@ -1,4 +1,18 @@
 
 public class Verb extends Word{
-
+	private String firstPP;
+	private String secondPP;
+	private String thirdPP;
+	private String fourthPP;
+	
+	public Verb(String firstPP, String secondPP, String thirdPP, String fourthPP){
+		this.firstPP = firstPP;
+		this.secondPP = secondPP;
+		this.thirdPP = thirdPP;
+		this.fourthPP = fourthPP;
+	}
+	
+	public int getConjugation(){
+		
+	}
 }
