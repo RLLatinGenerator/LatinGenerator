147a373ea0498dfab429b8eee584d4c751ef4bce
InfinityDeltaX <delta0357@gmail.com>
2015-01-11 22:00:18 -0500

    Noun should be OK.

--- LatinGenerator/Noun.java
+++ LatinGenerator/Noun.java
@@ -3,11 +3,14 @@ public class Noun extends Word{
 	private String base;
 	private int declension;
 	public static Noun(String nominative, String genitive){
-		
+		declension = 2;
+		base = genitive.substring(0, genitive.length()); //take off the last i.
 	}
 	
 	public ConjugatedNoun decline(int Case, int plurality){
-		return addEnding(Values.DECLENSION_SECOND[Case][plurality]);
+		if(declension == 2){
+			return addEnding(Values.DECLENSION_SECOND[Case][plurality]);
+		}
 	}
 	
 	public static String addEnding(String ending){
