a2dfecd4cb68670af38f176f37de4edeef569304
InfinityDeltaX <delta0357@gmail.com>
2015-01-11 21:46:13 -0500

    Added Values

---
+++ LatinGenerator/Values.java
@@ -0,0 +1,10 @@
+
+public class Values {
+	public static final int CASE_NOMINATIVE = 0;
+	public static final int CASE_GENTIVE = 0;
+	public static final int CASE_DATIVE= 0;
+	public static final int CASE_ACCUSATIVE= 0;
+	public static final int CASE_ABLATIVE = 0;
+	public static final int CASE_VOCATIVE= 0;
+	public static final int CASE_LOCATIVE = 0;
+}
