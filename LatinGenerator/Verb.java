6d85a86c5e65de9975ca5758f295be4aabff2098
KingSting <King15.767@gmail.com>
2015-01-11 20:29:39 -0500

    Commented

--- LatinGenerator/Verb.java
+++ LatinGenerator/Verb.java
@@ -1,9 +1,13 @@
 
 public class Verb extends Word{
 	private String firstPP;
+	//First principal part
 	private String secondPP;
+	//Second principal part
 	private String thirdPP;
+	//Third principal part
 	private String fourthPP;
+	//Fourth principal part
 	
 	public Verb(String firstPP, String secondPP, String thirdPP, String fourthPP){
 		this.firstPP = firstPP;
