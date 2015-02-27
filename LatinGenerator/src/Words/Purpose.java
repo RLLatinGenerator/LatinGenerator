d88278d9f42b38e6322e09bccfbc5cec4f84c457
InfinityDeltaX <delta0357@gmail.com>
2015-02-27 10:21:52 -0500

    Added Purpose.java to store purpose types.

---
+++ LatinGenerator/src/Words/Purpose.java
@@ -0,0 +1,11 @@
+package Words;
+
+public class Purpose {
+	
+	public static final int NOUN_SUBJECT = 100;
+	public static final int NOUN_OBJECT = 101;
+	public static final int NOUN_POSSESSION = 102;
+	public static final int NOUN_PREDICATE = 103;
+	
+	public static final int VERB_MAIN = 201;
+}
