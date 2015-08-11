499a1e8dbb76574bebac2edf782d832022e4f816
KingSting <king15.767@gmail.com>
2015-08-10 20:42:51 -0400

    got junit working

--- src/test/NounTest.java
+++ src/test/NounTest.java
@@ -14,7 +14,7 @@ public class NounTest {
 	@Test
 	public void testMulti() {
 		Noun noun = new Noun("nominativus", "gentivi", 5, Values.GENDER_MASCULINE, Values.INDEX_NOUN_TYPE_DECLENSION_SECOND,  new ArrayList<>(Arrays.asList("Derp")));
-		assertEquals("Nominative singular case is incorrect", "nominativus", noun.decline(101, Values.CASE_NOMINATIVE, Values.NUMBER_SINGULAR));
+		assertEquals("Nominative singular case is incorrect", "nominativus", noun.decline(101, Values.CASE_NOMINATIVE, Values.NUMBER_SINGULAR).getConjugatedWord());
 	  }
 	
 }
